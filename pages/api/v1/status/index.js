63d06de1794dd06e9ec63ef390c3f9baaf5bbc7f
Leonardo Martins <85245862+leodsmartins@users.noreply.github.com>
2026-02-03 12:44:52 +0000

    Add  and  files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,8 @@
-function status(request, response) {
-  console.log(response);
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1+1 as sum;");
+  console.log(result.rows);
   return response.status(200).json({ status: "ok" });
 }
 
